92b180660902eedcc0b39f680637c143be11c41c
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2022-07-26 23:29:34 -0300

    handles responses to /feed/posts

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,2 +1,7 @@
 //? Handles requests to website/feed/posts and returns JSON data
-exports.getPosts = (req, res, next) => {};
+exports.getPosts = (req, res, next) => {
+	//? Set the response status as 200 and return posts data
+	res.status(200).json({
+		posts: [{ title: 'First Post', content: 'This is the content body for the first post' }],
+	});
+};
