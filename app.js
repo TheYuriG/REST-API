18df40d7f146220993a7799be32c492dc41065d7
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2022-07-27 05:12:31 -0300

    handling CORS errors

--- app.js
+++ app.js
@@ -13,6 +13,17 @@ const restPort = 8080;
 //? Parse the JSON data sent in POST and PUT requests
 app.use(parser.json());
 
+//? Handles CORS issues
+app.use((req, res, next) => {
+	//? Configure the server to accept requests from any website
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	//? Configure the server to accept these specific request keywords
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+	//? Configure the server to accept headers for content and for authentication
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	next();
+});
+
 //? Use the routes
 app.use('/feed', feedRoutes);
 
