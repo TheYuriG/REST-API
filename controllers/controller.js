92fe07baadde0a5fa7dc62d3531b53f8f1fb74be
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2022-07-28 08:31:49 -0300

    fixed get posts API

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,7 +2,18 @@
 exports.getPosts = (req, res, next) => {
 	//? Set the response status as 200 and return posts data
 	res.status(200).json({
-		posts: [{ title: 'First Post', content: 'This is the content body for the first post' }],
+		posts: [
+			{
+				_id: '1',
+				title: 'First Post',
+				content: 'This is the content body for the first post',
+				imageUrl: 'images/farCry.jpg',
+				creator: {
+					name: 'You',
+				},
+				createdAt: new Date(),
+			},
+		],
 	});
 };
 
