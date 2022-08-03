dfb45d3c4231d3004ad03799d3807e3e5fbb5cd2
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2022-08-03 04:32:05 -0300

    enabled post editing with validation

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,6 +2,8 @@
 const { validationResult } = require('express-validator');
 //? Import our MongoDB posts Schema
 const Post = require('../models/post.js');
+//? Import the file deletion helper function
+const { deleteImage } = require('../util/delete-image.js');
 
 //? Handles GET requests to website/feed/posts and returns JSON data
 exports.getPosts = (req, res, next) => {
@@ -75,6 +77,7 @@ exports.postNewPost = (req, res, next) => {
 		});
 };
 
+//? Pulls additional data of a single post when clicking "View" on the feed
 exports.getSinglePostDetail = (req, res, next) => {
 	//? Fetch the post ID from the URL, it will be after "/post/"
 	const postId = req.params.postId;
@@ -104,6 +107,78 @@ exports.getSinglePostDetail = (req, res, next) => {
 		});
 };
 
-// exports.updatePost = (req, res, next) => {
-//     imageUrl = req.file.path.replace("\\","/");
-// }
+//? Opens a modal to update a post when clicking "Edit" in the feed
+exports.updatePost = (req, res, next) => {
+	//? Pull the errors (if any) from the post route
+	const errors = validationResult(req);
+
+	//? If there are errors, throw an error so the express error handler catches it
+	if (!errors.isEmpty()) {
+		const error = new Error('Validation error!');
+		error.statusCode = 422;
+		error.array = errors.array();
+		throw error;
+	}
+
+	//? If no errors, proceed with the proper success response and
+	//? store the new data on the database
+	const postId = req.params.postId;
+	const title = req.body.title;
+	const content = req.body.content;
+	let imageUrl = req.body.image;
+
+	//? If a file was uploaded, update the file location for Windows
+	if (req.file) {
+		imageUrl = req.file.path.replace('\\', '/');
+	}
+
+	//? Basic check if there an image exists
+	if (!imageUrl) {
+		const error = new Error('No file selected.');
+		error.statusCode = 422;
+		throw error;
+	}
+
+	//? Look up on the database for the post being searched
+	Post.findById(postId)
+		.then((post) => {
+			//! This will not find the post if the post was deleted between
+			//! the page load and the "Edit" click
+			if (!post) {
+				const error = new Error("Post wasn't found on the database to be updated.");
+				error.statusCode = 404;
+				error.array = errors.array();
+				throw error;
+			}
+
+			//? Update the database post with the newly provided data
+			let updatedPost = post;
+			post.title = title;
+			post.content = content;
+
+			//? Check if the image changed
+			if (imageUrl !== post.imageUrl) {
+				//? If the image changed, delete the old image from the server
+				deleteImage(post.imageUrl);
+				//? and update database with filePath to new image
+				post.imageUrl = imageUrl;
+			}
+
+			//? Save the updated post on the database again
+			return post.save();
+		})
+		.then((result) => {
+			//? Return a success message to the client
+			res.status(200).json({
+				message: 'Post successfully updated on the database!',
+				post: result,
+			});
+		})
+		.catch((err) => {
+			//? Forward the error to the express error handler
+			if (!err.statusCode) {
+				err.statusCode = 500;
+			}
+			next(err);
+		});
+};
--- routes/routes.js
+++ routes/routes.js
@@ -15,12 +15,21 @@ router.get('/posts', feedController.getPosts);
 //? Create a new post and save it in the server
 router.post(
 	'/post',
+	//? Validate data before passing to the new post controller
 	[body('title').trim().isLength({ min: 5 }), body('content').trim().isLength({ min: 5 })],
 	feedController.postNewPost
 );
 
 //? Post controller for the page that displays information about a single post
 router.get('/post/:postId', feedController.getSinglePostDetail);
+
+//? Put controller to edit posts
+router.put(
+	'/post/:postId',
+	//? Validate data before passing to the edit post controller
+	[body('title').trim().isLength({ min: 5 }), body('content').trim().isLength({ min: 5 })],
+	feedController.updatePost
+);
 
 //? Export back to app.js
 module.exports = router;
