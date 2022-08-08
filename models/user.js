544a06ed9bba296950272ffdae3319a39fa98c29
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2022-08-07 22:37:09 -0300

    created User model

---
+++ models/user.js
@@ -0,0 +1,15 @@
+//? Import mongoose to handle MongoDB
+const mongoose = require('mongoose');
+//? Import Schema from mongoose so we can define a data Schema for the users
+const Schema = mongoose.Schema;
+
+//? Create the data structure for posts
+const userSchema = new Schema({
+	email: { type: String, required: true },
+	password: { type: String, required: true },
+	name: { type: String, required: true },
+	status: { type: String, required: true },
+	posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
+});
+
+modules.export = mongoose.model('User', userSchema);
