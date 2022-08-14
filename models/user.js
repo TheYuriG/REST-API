7b5c78ad4bc57c65ab59e5fb24b1a339d790b9d9
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2022-08-14 19:38:54 -0300

    fixed export in User model

--- models/user.js
+++ models/user.js
@@ -12,4 +12,4 @@ const userSchema = new Schema({
 	posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
 });
 
-module.export = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema);
