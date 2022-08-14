9d528ec51793d5082f490d6dde8269d64702a5a5
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2022-08-14 20:11:32 -0300

    basic configuration for authentication (pre-JWT)

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -41,3 +41,58 @@ exports.register = (req, res, next) => {
 			next(err);
 		});
 };
+
+//? Handles POST requests to '/auth/authenticate'
+exports.authentication = (req, res, next) => {
+	//? Pull email and password from request body
+	const email = req.body.email;
+	const password = req.body.password;
+	//? Create a temp variable to store the user object returned from a
+	//? database check using the email
+	let loadedUser;
+
+	//? Look up if there is an user with the provided email
+	User.findOne({ email: email })
+		//? If the network connection to MongoDB works
+		.then((user) => {
+			//? Check if an user was found
+			if (!user) {
+				//? If no user was found, throw an error
+				const error = new Error('An user with this email could not be found.');
+				error.statusCode = 401;
+				throw error;
+			}
+
+			//? If an user was found, the IF block above will be skipped
+			//? so we have an user and store it on the temp variable we created
+			//? then we can use this user after comparing the provided and stored
+			//? passwords without making another request to the database
+			loadedUser = user;
+
+			//? Compare the provided and stored passwords
+			return bcrypt.compare(password, user.password);
+		})
+		.then((isEqual) => {
+			//? If bcrypt returns that the passwords do not match, throw an error
+			if (!isEqual) {
+				const error = new Error('Passwords do not match.');
+				error.statusCode = 401;
+				throw error;
+			}
+
+			//? If the passwords match, the IF block above gets skipped and
+			//? we authenticate the user
+			//! Install JWT and return that to the client to store
+			// res.status(201).json({
+			// 	message: 'User created!',
+			// 	userId: result._id,
+			// });
+		})
+		.catch((err) => {
+			//? Forward the error to the express error handler
+			if (!err.statusCode) {
+				err.statusCode = 500;
+			}
+			next(err);
+		});
+};
--- routes/auth.js
+++ routes/auth.js
@@ -25,5 +25,18 @@ router.put(
 	authController.register
 );
 
+//? Handles requests to login at domain + /auth/authenticate
+router.post(
+	'/authenticate',
+	//? Validate data with express-validator
+	[
+		//? Check if email is valid
+		body('email').isEmail().withMessage('Please enter a valid email.').normalizeEmail(),
+		//? Checks if password is at least 5 chars long after trimming
+		body('password').trim().isLength({ min: 5 }),
+	],
+	authController.authentication
+);
+
 //? Export back to app.js
 module.exports = router;
