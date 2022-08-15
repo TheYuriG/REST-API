bafe1359d38ef30c07f25f62082c9ec6d452e26b
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2022-08-15 11:37:48 -0300

    protecting post routes with authentication

--- routes/post.js
+++ routes/post.js
@@ -2,6 +2,8 @@
 const express = require('express');
 //? Import validation
 const { body } = require('express-validator');
+//? Import authentication middleware to handle validation of JWT tokens
+const isAuth = require('../util/is-auth.js');
 
 //? Import the posts feed controller
 const feedController = require('../controllers/controller.js');
@@ -10,29 +12,31 @@ const feedController = require('../controllers/controller.js');
 const router = express.Router();
 
 //? Load posts stored in the server
-router.get('/posts', feedController.getPosts);
+router.get('/posts', isAuth, feedController.getPosts);
 
 //? Create a new post and save it in the server
 router.post(
 	'/post',
+	isAuth,
 	//? Validate data before passing to the new post controller
 	[body('title').trim().isLength({ min: 5 }), body('content').trim().isLength({ min: 5 })],
 	feedController.postNewPost
 );
 
 //? Post controller for the page that displays information about a single post
-router.get('/post/:postId', feedController.getSinglePostDetail);
+router.get('/post/:postId', isAuth, feedController.getSinglePostDetail);
 
 //? Put controller to edit posts
 router.put(
 	'/post/:postId',
+	isAuth,
 	//? Validate data before passing to the edit post controller
 	[body('title').trim().isLength({ min: 5 }), body('content').trim().isLength({ min: 5 })],
 	feedController.updatePost
 );
 
 //? Delete controller to delete posts
-router.delete('/post/:postId', feedController.deletePost);
+router.delete('/post/:postId', isAuth, feedController.deletePost);
 
 //? Export back to app.js
 module.exports = router;
