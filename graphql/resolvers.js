7d19df6a246a1b6c88369245e5fde4b509ccc97e
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2022-08-26 19:38:17 -0300

    properly commented resolver function

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,20 +1,36 @@
+//? Import encryption NPM package
 const bcrypt = require('bcryptjs');
-const User = require('../models/user.js');
 
+//? Import data models
+const User = require('../models/user.js');
+const Post = require('../models/post.js');
+
+//? Export functions to be used by GraphQL resolver
 module.exports = {
+	//? Function to create an user on the database when user accesses
+	//? '/signup' and sends a valid form
 	createUser: async function ({ userInput: { name, password, email } }, req) {
+		//? First we check if an user already exists with this email
 		const existingUser = await User.findOne({ email: email });
+		//? If we found an user, throw an error
 		if (existingUser) {
 			const error = new Error('User already exists!');
 			throw error;
 		}
+
+		//? If there is no user with that email, hash the password they gave us
 		const hashedPassword = await bcrypt.hash(password, 12);
+
+		//? Create a new user with the data they sent and the hashed password
 		const user = new User({
 			email: email,
 			password: hashedPassword,
 			name: name,
 		});
+		//? Save this new user to the database
 		const savedUser = await user.save();
+
+		//? Return to GraphQL the data it needs to separate and send to the client
 		return { ...savedUser._doc, _id: savedUser._id.toString() };
 	},
 };
