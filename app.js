bbacac232ff5ccec9a8777ddf9d5b0f4b2934fd2
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2022-09-09 22:00:49 -0300

    reverted GraphQL to REST for testing

--- app.js
+++ app.js
@@ -3,18 +3,9 @@ const express = require('express');
 const parser = require('body-parser');
 const mongoose = require('mongoose');
 
-//? Import GraphQL-related files
-const { graphqlHTTP } = require('express-graphql');
-const graphqlSchema = require('./graphql/schema.js');
-const graphqlResolver = require('./graphql/resolvers.js');
-
 //? Import all the multer parsing logic from separate file to avoid
 //? cluttering main file for no reason. This will handle image uploads
 const { multerParser } = require('./util/multer-filter.js');
-//? Import the file deletion helper function
-const { deleteImage } = require('./util/delete-image.js');
-//? Import graphQL authentication module
-const auth = require('./util/is-auth.js');
 
 //? Import path node module to serve images from "/images" to the front end
 const path = require('path');
@@ -22,6 +13,10 @@ const path = require('path');
 //! Import the API key for the MongoDB storage.
 //! You need to create your own key if you are cloning this project
 const { mongoKey } = require('./util/secrets/keys.js');
+
+//? Import the routes
+const feedRoutes = require('./routes/post');
+const authenticationRoutes = require('./routes/auth');
 
 //? Initialize the app
 const app = express();
@@ -33,6 +28,9 @@ app.use(parser.json());
 //? Parse and store files using multer
 app.use(multerParser);
 
+//? Serves images to the frontend
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
 //? Handles CORS issues
 app.use((req, res, next) => {
 	//? Configure the server to accept requests from any website
@@ -41,90 +39,12 @@ app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
 	//? Configure the server to accept headers for content and for authentication
 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-	//? Send a success message to every OPTIONS HTTP request. This is specially
-	//? important to deal with GraphQL requests
-	if (req.method === 'OPTIONS') {
-		return res.sendStatus(200);
-	}
 	next();
 });
 
-//? REST endpoint to deal with image uploads, since GraphQL can't
-app.put(
-	//? Endpoint path
-	'/uploads',
-	//? Handles and processes every request and determine if they
-	//? come from an authenticated user or not
-	auth,
-	(req, res, next) => {
-		//? If the user is trying to send a PUT request without being logged
-		//? in, return them an error
-		if (!req.isAuth) {
-			throw new Error('Not authenticated!');
-		}
-		//? Check if this request has an image in it
-		if (!req.file) {
-			//? If no image, return a positive response stating no image was
-			//? uploaded. This covers both failed upload attempts which will
-			//? give a post creation error and also post editing where there
-			//? is no need to change the already used image
-			return res.status(200).json({ message: 'No file provided!' });
-		}
-
-		//? If we are editing a post and a new image is being sent, we need to
-		//? delete the old image from the storage
-		if (req.body.oldPath) {
-			deleteImage(req.body.oldPath);
-		}
-
-		//? After completing these checks and possible operations, return a
-		//? success message with the path route for the uploaded image
-		return res
-			.status(201)
-			.json({
-				message: 'File stored successfully',
-				filePath: req.file.path.replace('\\', '/'),
-			});
-	}
-);
-
-//? Serves images to the frontend
-app.use('/images', express.static(path.join(__dirname, 'images')));
-
-//? Handling all GraphQL requests in one place
-app.use(
-	//? To which route requests should be read as GraphQL requests
-	'/graphql',
-	//? Handles and processes every request and determine if they
-	//? come from an authenticated user or not
-	auth,
-	//? Use GraphQL middleware
-	graphqlHTTP({
-		//? Where the data schema is defined
-		schema: graphqlSchema,
-		//? Where is the resolver for queries
-		rootValue: graphqlResolver,
-		//? Enable access to server + /graphiql for visual
-		//? representation of being sent and received
-		graphiql: true,
-		//? GraphQL error handling function
-		customFormatErrorFn(thrownError) {
-			//? Checks if the error doesn't have an original error that is
-			//? automatically detected by GraphQL, created by us or
-			//? a third party package we could be using
-			if (!thrownError.originalError) {
-				return thrownError;
-			}
-			//? If there is, we assume this is a controlled, manually
-			//? crafted error. Destructure and extract its data and return
-			//? the error to the frontend
-			const {
-				originalError: { data, message = 'An error occurred', statusCode = 500 },
-			} = thrownError;
-			return { message: message, statusCode: statusCode, data: data };
-		},
-	})
-);
+//? Use the routes
+app.use('/feed', feedRoutes);
+app.use('/auth', authenticationRoutes);
 
 //? General error handler for any thrown errors inside express
 app.use((error, req, res, next) => {
@@ -145,8 +65,14 @@ mongoose
 	.connect(mongoKey)
 	.then(() => {
 		//? Start the server
-		app.listen(restPort);
-		console.log('Connected to MongoDB and server is online!');
+		const server = app.listen(restPort);
+		//? Initialize the websocket with Socket.IO and pass the express server
+		//? into it so it can be used
+		const io = require('./util/socket.js').init(server);
+		//! This is meant to inform us on every client connection to the
+		//! websocket. Commented out because it's only useful for debugging
+		// io.on('connection', (socket) => {});
+		console.log('Connected to MongoDB! Socket.IO and server are online!');
 	})
 	.catch(() => {
 		console.log('Error connecting to MongoDB');
---
+++ controllers/feedController.js
@@ -0,0 +1,275 @@
+//? Import validation to check if the routes are giving data errors
+const { validationResult } = require('express-validator');
+//? Import our MongoDB posts Schema
+const Post = require('../models/post.js');
+const User = require('../models/user.js');
+//? Import the file deletion helper function
+const { deleteImage } = require('../util/delete-image.js');
+//? Import Socket.IO websocket controller
+const io = require('../util/socket.js');
+
+//? Handles GET requests to website/feed/posts and returns JSON data
+exports.getPosts = async (req, res, next) => {
+	//? Pull the page number to work with proper pagination
+	const currentPage = req.query.page || 1;
+	//? Sets the limit of items to be displayed per page
+	const postLimitPerPage = 10;
+
+	try {
+		//? Query the database for the number of items total
+		const totalPosts = await Post.find().countDocuments();
+
+		//? Access the database to pull all posts
+		const posts = await Post.find()
+			.populate('creator')
+			.sort({ createdAt: -1 })
+			.skip((currentPage - 1) * postLimitPerPage)
+			.limit(postLimitPerPage);
+		//? Set the response status as 200 and return posts data and
+		//? the total number of posts so the frontend can properly paginate
+		res.status(200).json({
+			message: 'Posts fetched',
+			posts: posts,
+			totalItems: totalPosts,
+		});
+	} catch (err) {
+		//? Forward the error to the express error handler
+		if (!err.statusCode) {
+			err.statusCode = 500;
+		}
+		next(err);
+	}
+};
+
+//? Handles POST requests to create new posts at website/feed/post
+exports.postNewPost = async (req, res, next) => {
+	//? Pull the errors (if any) from the post route
+	const errors = validationResult(req);
+
+	//? If there are errors, throw an error so the express error handler catches it
+	if (!errors.isEmpty()) {
+		const error = new Error('Validation error!');
+		error.statusCode = 422;
+		error.array = errors.array();
+		throw error;
+	}
+
+	//? Check if a proper image was uploaded
+	if (!req.file) {
+		//? Throw an error if no proper image was sent
+		const error = new Error('No image provided!');
+		error.statusCode = 422;
+		throw error;
+	}
+
+	//? If no errors, proceed with the proper success response and database storage
+	const title = req.body.title;
+	const content = req.body.content;
+	const imageUrl = req.file.path.replace('\\', '/');
+
+	//? Create a new post in the database following our Schema
+	const post = new Post({
+		title: title,
+		content: content,
+		imageUrl: imageUrl,
+		creator: req.userId, //? We store the logged ID in the request using the 'is-auth.js' middleware
+	});
+
+	try {
+		//? Save this post in the database
+		await post.save();
+
+		//? After saving the post, fetch the user
+		const user = await User.findById(req.userId);
+
+		//? Once you have the user, add this post to the user's posts on the database
+		user.posts.push(post);
+		await user.save();
+
+		//? Sends event to websocket so the clients will update their UI with the newly fetched data
+		io.getIO().emit('posts', {
+			action: 'create',
+			post: { ...post._doc, creator: { _id: req.userId, name: user.name } },
+		});
+
+		//? After both operations are successful, return a success response to the client
+		res.status(201).json({
+			message: 'Post created successfully!',
+			post: post,
+			creator: { _id: user._id, name: user.name },
+		});
+	} catch (err) {
+		//? Forward the error to the express error handler
+		if (!err.statusCode) {
+			err.statusCode = 500;
+		}
+		next(err);
+	}
+};
+
+//? Pulls additional data of a single post when clicking "View" on the feed
+exports.getSinglePostDetail = async (req, res, next) => {
+	//? Fetch the post ID from the URL, it will be after "/post/"
+	const postId = req.params.postId;
+
+	try {
+		//? Look up the database if there is an post with this same ID extracted
+		const post = await Post.findById(postId).populate('creator');
+		//? If nothing was found, throw an error for the following catch block
+		if (!post) {
+			const error = new Error('Could not find post');
+			error.statusCode = 404;
+			throw error;
+		}
+
+		//? If something was found, return a success response
+		res.status(200).json({
+			message: 'Post Fetched',
+			post: post,
+		});
+	} catch (err) {
+		//? Forward the error to the express error handler
+		if (!err.statusCode) {
+			err.statusCode = 500;
+		}
+		next(err);
+	}
+};
+
+//? Opens a modal to update a post when clicking "Edit" in the feed
+exports.updatePost = async (req, res, next) => {
+	//? Pull the errors (if any) from the post route
+	const errors = validationResult(req);
+
+	//? If there are errors, throw an error so the express error handler catches it
+	if (!errors.isEmpty()) {
+		const error = new Error('Validation error!');
+		error.statusCode = 422;
+		error.array = errors.array();
+		throw error;
+	}
+
+	//? If no errors, proceed with the proper success response and
+	//? store the new data on the database
+	const postId = req.params.postId;
+	const title = req.body.title;
+	const content = req.body.content;
+	let imageUrl = req.body.image;
+
+	//? If a file was uploaded, update the file location for Windows
+	if (req.file) {
+		imageUrl = req.file.path.replace('\\', '/');
+	}
+
+	//? Basic check if there an image exists
+	if (!imageUrl) {
+		const error = new Error('No file selected.');
+		error.statusCode = 422;
+		throw error;
+	}
+
+	try {
+		//? Look up on the database for the post being searched
+		const post = await Post.findById(postId).populate('creator');
+		//! This will not find the post if the post was deleted between
+		//! the page load and the "Edit" click
+		if (!post) {
+			const error = new Error("Post wasn't found on the database to be updated.");
+			error.statusCode = 404;
+			error.array = errors.array();
+			throw error;
+		}
+
+		//? Check if the person trying to edit the post is the
+		//? same person who created it and fail the request if isn't
+		if (post.creator._id.toString() !== req.userId) {
+			const error = new Error('Not authorized!');
+			error.statusCode = 403;
+			throw error;
+		}
+
+		//? Update the database post with the newly provided data
+		post.title = title;
+		post.content = content;
+
+		//? Check if the image changed
+		if (imageUrl !== post.imageUrl) {
+			//? If the image changed, delete the old image from the server
+			deleteImage(post.imageUrl);
+			//? and update database with filePath to new image
+			post.imageUrl = imageUrl;
+		}
+
+		//? Save the updated post on the database again
+		const savedPost = await post.save();
+
+		//? Sends event to websocket so the clients will update their UI with
+		//? the recently updated post
+		io.getIO().emit('posts', {
+			action: 'update',
+			post: savedPost,
+		});
+
+		//? Return a success message to the client
+		res.status(200).json({
+			message: 'Post successfully updated on the database!',
+			post: savedPost,
+		});
+	} catch (err) {
+		//? Forward the error to the express error handler
+		if (!err.statusCode) {
+			err.statusCode = 500;
+		}
+		next(err);
+	}
+};
+
+//? Delete the post, if the user has delete rights to it
+exports.deletePost = async (req, res, next) => {
+	//? Extract the post ID from the URL
+	const postId = req.params.postId;
+
+	try {
+		//? Look up if this post actually exists and if the person has delete permissions for it
+		const post = await Post.findById(postId);
+		//? If no post was found, throw error
+		if (!post) {
+			const error = new Error('Could not find post to delete');
+			error.statusCode = 404;
+			throw error;
+		}
+
+		//? Check if the person trying to delete the post is the
+		//? same person who created it and fail the request if isn't
+		if (post?.creator.toString() !== req.userId) {
+			const error = new Error('Not authorized!');
+			error.statusCode = 403;
+			throw error;
+		}
+
+		//? Searches and destroys this post on the database
+		await Post.findByIdAndDelete(postId);
+		//? Once the post was destroyed, we need to remove this post reference
+		//? from the user's posts. First we fetch the user
+		const user = await User.findById(req.userId);
+		//? Then we remove the post reference from the 'posts' array inside
+		//? the user entry on the database
+		user.posts.pull(postId);
+		//? then we save this user without the post reference
+		await user.save();
+
+		//? Emit an event on the websocket
+		io.getIO().emit('posts', { action: 'delete', post: postId });
+
+		//? Finally delete the image from the server storage
+		deleteImage(post.imageUrl);
+		//? Return a success message to the user
+		res.status(200).json({ message: 'Successful post deletion!' });
+	} catch (err) {
+		//? Forward the error to the express error handler
+		if (!err.statusCode) {
+			err.statusCode = 500;
+		}
+		next(err);
+	}
+};
